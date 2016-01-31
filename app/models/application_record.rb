5b34d1ecaf96c5a4c423e9e10b24724448cba06f
Tom Prats <tprats108@gmail.com>
2016-01-31 04:10:57 -0500

    Time + Utilities

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.date_from(*attrs)
+    attrs.each do |attr|
+      define_method("#{attr}_date") do
+        value = read_attribute attr
+        value && value.strftime("%m/%d/%y %I:%M %p")
+      end
+    end
+  end
+
+  date_from :created_at, :updated_at
 end
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = "Eastern Time (US & Canada)"
+    config.autoload_paths += Dir[Rails.root.join("app", "models", "{*/}")]
   end
 end
