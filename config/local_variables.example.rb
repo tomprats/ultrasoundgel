22f4e4a55acf71b582e630eb7e879e42823d2f10
Tom Prats <tprats108@gmail.com>
2016-02-22 06:48:07 -0500

    Assets

--- config/local_variables.example.rb
+++
@@ -1,9 +0,0 @@
-# Example
-ENV["APP"]            = "Default"
-ENV["TITLE"]          = "Default"
-ENV["DESCRIPTION"]    = "Default Setup"
-ENV["EMAIL"]          = "tom@tomprats.com"
-ENV["FACEBOOK"]       = "tomify.me"
-ENV["AWS_BUCKET"]     = ""
-ENV["AWS_ACCESS_KEY"] = ""
-ENV["AWS_SECRET_KEY"] = ""
