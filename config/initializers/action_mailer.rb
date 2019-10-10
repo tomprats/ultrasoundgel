842f2b174695e87d02d457dc8fd6a224d3a9cc71
Tom Prats <tprats108@gmail.com>
2019-10-10 03:52:18 -0400

    Fixed app issues

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,4 +1,6 @@
 if Rails.env.development?
+  Rails.application.config.action_mailer.preview_path = "#{Rails.root}/lib/previews"
+else
   Rails.application.config.action_mailer.smtp_settings = {
     address: "email-smtp.us-east-1.amazonaws.com",
     port: "587",
@@ -8,6 +10,4 @@ if Rails.env.development?
     authentication: "plain",
     enable_starttls_auto: true
   }
-
-  Rails.application.config.action_mailer.preview_path = "#{Rails.root}/lib/previews"
 end
